6726a9860633e7b2ee5f20e678104cfc129f07f7
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-10 11:39:37 +0700

    Update script bundle

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -7,6 +7,22 @@ namespace GardenShopOnline
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new MyScriptBundle("~/bundles/js").Include(
+                "~/assets/js/vendor/bootstrap.bundle.min.js",
+                "~/assets/js/vendor/jquery-3.6.0.min.js",
+                "~/assets/js/vendor/jquery-migrate-3.3.2.min.js",
+                "~/assets/js/vendor/jquery.waypoints.js",
+                "~/assets/js/vendor/modernizr-3.11.2.min.js",
+                "~/assets/js/plugins/wow.min.js",
+                "~/assets/js/plugins/swiper-bundle.min.js",
+                "~/assets/js/plugins/jquery.nice-select.js",
+                "~/assets/js/plugins/parallax.min.js",
+                "~/assets/js/plugins/jquery.magnific-popup.min.js",
+                "~/assets/js/plugins/tippy.min.js",
+                "~/assets/js/plugins/ion.rangeSlider.min.js",
+                "~/assets/js/plugins/mailchimp-ajax.js",
+                "~/assets/js/plugins/jquery.counterup.js"));
+
             bundles.Add(new MyStyleBundle("~/Content/css").Include(
                 "~/assets/css/bootstrap.min.css",
                 "~/assets/css/animate.min.css",
