68442a7b8707281d3d97fe22eb1593bce5dbb26b
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-10 10:44:34 +0700

    Run code cleanup for project

--- App_Start/Startup.Auth.cs
+++ App_Start/Startup.Auth.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿using GardenShopOnline.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Owin;
-using GardenShopOnline.Models;
+using System;
 
 namespace GardenShopOnline
 {
@@ -34,7 +33,7 @@ namespace GardenShopOnline
                         validateInterval: TimeSpan.FromMinutes(30),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
-            });            
+            });
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
