792541942237ce1c6f6500eddefff5824941690b
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-10 11:32:10 +0700

    Update style bundle for project

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -7,6 +7,15 @@ namespace GardenShopOnline
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new MyStyleBundle("~/Content/css").Include(
+                "~/assets/css/bootstrap.min.css",
+                "~/assets/css/animate.min.css",
+                "~/assets/css/swiper-bundle.min.css",
+                "~/assets/css/nice-select.css",
+                "~/assets/css/magnific-popup.min.css",
+                "~/assets/css/ion.rangeSlider.min.css"));
+
+            BundleTable.EnableOptimizations= true;
         }
     }
 }
