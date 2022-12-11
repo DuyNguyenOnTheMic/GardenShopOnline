123198209c7ae758868106898ac9d21281081436
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-11 13:55:23 +0700

    Add shopping cart view models

---
+++ Models/ShoppingCartViewModels.cs
@@ -0,0 +1,11 @@
+﻿using GardenShopOnline.Models;
+using System.Collections.Generic;
+
+namespace GardenShopOnline.ViewModels
+{
+    public class ShoppingCartViewModels
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
