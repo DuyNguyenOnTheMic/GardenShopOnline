da07944dce2d269e18ca2eaa57cc875f6ed6e465
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-12 20:27:57 +0700

    Update mini cart ajax update

--- assets/customer/js/mini-cart.js
+++ assets/customer/js/mini-cart.js
@@ -10,4 +10,18 @@ function getMiniCart() {
     $.get(url, function (data) {
         miniCartList.html(data);
     });
+
+    $(document).off('click', '.product-item_remove').on('click', '.product-item_remove', function (e) {
+        e.preventDefault();
+        // Get the id from the link
+        var recordToDelete = $(this).attr('data-id');
+        if (recordToDelete != '') {
+            // Perform the ajax post
+            $.post('/ShoppingCart/RemoveProduct', { 'id': recordToDelete },
+                function (data) {
+                    $('#miniRow-' + data.DeleteId).fadeOut('slow');
+                    $('.minicart-item_total .ammount').text(data.CartTotal);
+                });
+        }
+    });
 }
