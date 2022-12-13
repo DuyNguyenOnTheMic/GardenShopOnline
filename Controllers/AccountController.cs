0745c3e97524b0c34a6c94b974b42b4aa8f9d907
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-13 19:45:04 +0700

    Update migrate shopping cart on register

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -69,14 +69,13 @@ namespace GardenShopOnline.Controllers
                 return View(model);
             }
 
-            MigrateShoppingCart(model.Email);
-
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
+                    MigrateShoppingCart(model.Email);
                     return RedirectToLocal(returnUrl);
                 case SignInStatus.LockedOut:
                     return View("Lockout");
@@ -109,6 +108,7 @@ namespace GardenShopOnline.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                    MigrateShoppingCart(model.Email);
 
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
