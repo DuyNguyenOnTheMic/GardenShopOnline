53eb6ddeae228c00e0b7410c273e39b125501718
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-14 22:44:47 +0700

    Update send email code

--- App_Start/IdentityConfig.cs
+++ App_Start/IdentityConfig.cs
@@ -25,13 +25,12 @@ namespace GardenShopOnline
         {
             var apiKey = Environment.GetEnvironmentVariable("BonsaiGarden");
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("bonsaigardenshop6@gmail.com", "Bonsai Garden");
+            var from = new EmailAddress("bonsaigardenshop6@gmail.com");
             var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("duy.197pm21905@vanlanguni.vn");
+            var to = new EmailAddress("duy940210714@gmail.com");
             var plainTextContent = "and easy to do anywhere, even with C#";
             var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
 
             // Send the email.
             if (client != null)
