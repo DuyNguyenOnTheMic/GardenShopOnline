f70f11cc675b2c1302402cd1dccf46c9f6c159e3
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-15 11:24:52 +0700

    Update fix bug migrate cart

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -108,8 +108,8 @@ namespace GardenShopOnline.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
                     MigrateShoppingCart(model.Email);
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
