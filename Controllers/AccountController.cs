85a7a76319fa8c9f827a029b5eca1b5fe0d0a9be
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-15 21:34:29 +0700

    Update confirm email html template again

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -115,7 +115,7 @@ namespace GardenShopOnline.Controllers
                     // Send an email with this link
                     string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                    await UserManager.SendEmailAsync(user.Id, "Confirm your account", callbackUrl);
 
                     ViewBag.Link = callbackUrl;
                     return View("DisplayEmail");
