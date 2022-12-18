f3bad01cfc099af14d11ce5b7eb844c4cee9dbe8
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-18 17:04:08 +0700

    Update statistics view

---
+++ Controllers/StatisticsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace GardenShopOnline.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: Statistics/Order
+        public ActionResult Order()
+        {
+            return View();
+        }
+    }
+}
