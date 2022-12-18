b1a6718cd330f84de30031eeeaa99b8ced5856ac
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-18 19:53:24 +0700

    Update function to get data

--- Controllers/StatisticsController.cs
+++ Controllers/StatisticsController.cs
@@ -1,13 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using GardenShopOnline.Models;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace GardenShopOnline.Controllers
 {
     public class StatisticsController : Controller
     {
+        readonly BonsaiGardenEntities db = new BonsaiGardenEntities();
+
         // GET: Statistics/Order
         public ActionResult Order()
         {
             return View();
+        }
+
+        public JsonResult GetOrderData()
+        {
+            return Json(db.CustomerOrders.Count(o => DbFunctions.TruncateTime(o.DateCreated) == DateTime.Today), JsonRequestBehavior.AllowGet);
         }
     }
 }
