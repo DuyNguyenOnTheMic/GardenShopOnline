dd80c01dfd6295fe4d055c938a8a8b4f608b644f
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-24 15:40:50 +0700

    Update chat html page

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace GardenShopOnline.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
