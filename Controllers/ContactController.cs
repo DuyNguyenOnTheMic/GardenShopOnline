9507e3b5c804231113749d5a0a8ad9249f9f7071
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-24 16:16:37 +0700

    Update send message action

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,12 +1,33 @@
-﻿using System.Web.Mvc;
+﻿using GardenShopOnline.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Web.Mvc;
 
 namespace GardenShopOnline.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly BonsaiGardenEntities db = new BonsaiGardenEntities();
+
         // GET: Contact
+        [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendMessage(string message, string toUserId)
+        {
+            var fromUserId = User.Identity.GetUserId();
+            db.Messages.Add(new Message()
+            {
+                FromUserId = fromUserId,
+                ToUserId = toUserId,
+                Message1 = message,
+                Status = 1,
+                DateCreated = DateTime.Now
+            });
             return View();
         }
     }
