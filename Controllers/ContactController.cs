fb1b86748979c82faf1e95efc9bfbd704c382923
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-24 16:48:56 +0700

    Update post message to server

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,19 +1,45 @@
 ﻿using GardenShopOnline.Hubs;
 using GardenShopOnline.Models;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using System;
+using System.Web;
 using System.Web.Mvc;
 
 namespace GardenShopOnline.Controllers
 {
     public class ContactController : Controller
     {
+        private ApplicationUserManager _userManager;
         private readonly BonsaiGardenEntities db = new BonsaiGardenEntities();
+
+        public ContactController()
+        {
+        }
+
+        public ContactController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
 
         // GET: Contact
         [HttpGet]
         public ActionResult Index()
         {
+            var adminUser = UserManager.FindByEmail("bonsaigarden6@gmail.com").Id;
+            ViewData["ToUserId"] = adminUser;
             return View();
         }
 
@@ -30,6 +56,7 @@ namespace GardenShopOnline.Controllers
                 Status = 1,
                 DateCreated = DateTime.Now
             });
+            db.SaveChanges();
             ChatHub.Send("blabla", message);
             return Json(new { success = true }, JsonRequestBehavior.AllowGet);
         }
