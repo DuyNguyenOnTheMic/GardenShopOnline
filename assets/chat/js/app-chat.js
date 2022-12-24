53fbe58f3b5da4a77600cb420fcaceb5223723c6
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-24 20:05:04 +0700

    Update app chat js

---
+++ assets/chat/js/app-chat.js
@@ -0,0 +1,48 @@
+﻿$(function () {
+    // Reference the auto-generated proxy for the hub.
+    var chat = $.connection.chatHub;
+    // Create a function that the hub can call back to display messages.
+    chat.client.addNewMessageToPage = function (name, message) {
+        // Add the message to the page.
+        /*$('#discussion').prepend('<li><strong>' + htmlEncode(name)
+            + '</strong>: ' + htmlEncode(message) + '</li>');*/
+    };
+    $.connection.hub.start().done(function () {
+        console.log(chat.connection.id);
+        $('#formMessage').submit(function (e) {
+            // Prevent default form submit
+            e.preventDefault();
+
+            var form = $(this);
+            var actionUrl = form.attr('action');
+            var message = $('#message').val();
+            var fromUserId = form.data('fromuserid');
+            var toUserId = form.data('touserid');
+            // Call the Send method on the hub.
+            $.ajax({
+                url: actionUrl,
+                type: 'POST',
+                data: { message, fromUserId, toUserId },
+                success: function (response) {
+                    if (response.success) {
+                        // Add chat message
+                        $('#discussion').prepend('<li class="chat-right">'
+                            + '<div class="chat-hour">' + response.time + '<span class="fa fa-check-circle ms-1"></span></div>'
+                            + '<div class="chat-text">' + htmlEncode(message) + '</div>'
+                            + '<div class="chat-avatar"</div>'
+                            + '</li>');
+                    } else {
+                        alert('An error has occured, please try again later!')
+                    }
+                }
+            });
+            // Clear text box and reset focus for next comment.
+            $('#message').val('').focus();
+        });
+    });
+});
+// This optional function html-encodes messages for display in the page.
+function htmlEncode(value) {
+    var encodedValue = $('<div />').text(value).html();
+    return encodedValue;
+}
