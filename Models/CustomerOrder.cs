1713c62a3231f219cf8380dec5a0433a8dbb5e33
QuocNam <tranquocnam201@gmail.com>
2022-12-28 14:00:19 +0700

    nhanwstin hình ảnh

--- Models/CustomerOrder.cs
+++ Models/CustomerOrder.cs
@@ -39,7 +39,9 @@ namespace GardenShopOnline.Models
         public decimal Total { get; set; }
         public int Status { get; set; }
         public string Reason { get; set; }
-    
+        public Nullable<decimal> PaidAdvance { get; set; }
+        public string Note { get; set; }
+
         public virtual AspNetUser AspNetUser { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
--- Models/Model1.Context.cs
+++ Models/Model1.Context.cs
@@ -37,7 +37,6 @@ namespace GardenShopOnline.Models
         public virtual DbSet<Message> Messages { get; set; }
         public virtual DbSet<OrderDetail> OrderDetails { get; set; }
         public virtual DbSet<Product> Products { get; set; }
-        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<Type> Types { get; set; }
     }
 }
