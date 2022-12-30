3d014e8a763b46df32dd41c0dfca4c25084593f7
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-30 10:03:11 +0700

    Update admin login page

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace GardenShopOnline.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
