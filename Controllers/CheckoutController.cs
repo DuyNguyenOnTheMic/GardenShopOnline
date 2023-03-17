4864e64e32da1979bb300602e731cd4c2793d9b9
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-03-17 15:32:06 +0700

    Update payment render in checkout view

--- Controllers/CheckoutController.cs
+++ Controllers/CheckoutController.cs
@@ -43,6 +43,7 @@ namespace GardenShopOnline.Controllers
             ViewData["FullName"] = user.FullName;
             ViewData["Address"] = user.Address;
             ViewData["Phone"] = user.PhoneNumber;
+            ViewBag.BankPayments = db.BankPayments.Where(b => b.Status == 1).ToList();
             return View();
         }
 
