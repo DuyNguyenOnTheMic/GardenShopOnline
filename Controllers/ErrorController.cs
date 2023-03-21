1dd7445e688f1fe8c62118bc8b74dfaddd4c8455
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-03-21 11:47:20 +0700

    Update default erro page

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace GardenShopOnline.Controllers
 {
     public class ErrorController : Controller
     {
-        // GET: Error
-        public ActionResult Index()
+        // GET: NotFound
+        public ActionResult NotFound()
         {
-            return View();
+            return View("NotFound");
         }
     }
 }
