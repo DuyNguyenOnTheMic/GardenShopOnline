959986ca98b21f1e8d55ddad48157dd1f00f36aa
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-05-19 12:04:03 +0700

    Update check for quantity when add to cart

--- Models/ShoppingCart.cs
+++ Models/ShoppingCart.cs
@@ -120,11 +120,22 @@ namespace GardenShopOnline.Models
             return db.Carts.Where(
                 cart => cart.ID == ShoppingCartId).ToList();
         }
+
         public int GetCount()
         {
             // Get the count of each item in the cart and sum them up
             int? count = (from cartItems in db.Carts
                           where cartItems.ID == ShoppingCartId
+                          select (int?)cartItems.Count).Sum();
+            // Return 0 if all entries are null
+            return count ?? 0;
+        }
+
+        public int GetItemCount(int id)
+        {
+            // Get the count of each item in the cart and sum them up
+            int? count = (from cartItems in db.Carts
+                          where cartItems.ID == ShoppingCartId && cartItems.ProductID == id
                           select (int?)cartItems.Count).Sum();
             // Return 0 if all entries are null
             return count ?? 0;
